8f7990d29058019b7d5ceee63e56079bc9dc541f
pkbhowmick <pkbhowmick007@gmail.com>
2020-04-02 20:10:24 +0600

    First commit

---
+++ flask-backend/static/react/precache-manifest.0ca92c21e66e7a447ed961c6a602a19d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d9543bab89b695c4c7e88b10c0d68d1",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "33dac54ec5c55d3261c0",
+    "url": "/static/react/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "052c5e5038159ab9583f",
+    "url": "/static/react/js/2.86f878de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/react/js/2.86f878de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33dac54ec5c55d3261c0",
+    "url": "/static/react/js/main.bfe4f819.chunk.js"
+  },
+  {
+    "revision": "c7241993fd2c7bed7213",
+    "url": "/static/react/js/runtime-main.b9f37f5d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
