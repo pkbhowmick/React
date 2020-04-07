2fd5b079cd761eeb56ba33e5b9a43689a24648d8
pkbhowmick <pkbhowmick007@gmail.com>
2020-04-07 18:38:43 +0600

    Create UI for taking Webcam pic

---
+++ flask-backend/static/react/precache-manifest.b830434b1f1fe0c667e5449d3953efb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b86d13f7447886737272e4602abf1ec",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "ee94d9a31c474ee0d9ee",
+    "url": "/static/react/css/main.3999a4bc.chunk.css"
+  },
+  {
+    "revision": "3b0b04ec2edbb5a3d2a4",
+    "url": "/static/react/js/2.4475f414.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/react/js/2.4475f414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee94d9a31c474ee0d9ee",
+    "url": "/static/react/js/main.a0472fa3.chunk.js"
+  },
+  {
+    "revision": "c7241993fd2c7bed7213",
+    "url": "/static/react/js/runtime-main.b9f37f5d.js"
+  }
+]);
---
+++ react-frontend/src/components/PageSwitcher.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom';
+
+class PageSwitcher extends Component {
+    render() {
+        return (
+            <div className="PageSwitcher">
+                <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
+                <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+            </div>
+
+            <div className="FormTitle">
+                <NavLink to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink>
+                <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
+            </div>
+        );
+    }
+}
+export default PageSwitcher;
